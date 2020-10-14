d82aa3085eb6019965dc1f003d8a160a96a93e0a
aambrioso <aambrioso1@gmail.com>
2020-10-14 18:51:20 -0400

    Modify app.py with servier = Flask....

--- app_Dash.py
+++ app_Dash.py
@@ -1,6 +1,7 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
+import flask
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
@@ -8,6 +9,7 @@ import dash_html_components as html
 import plot_module as pm
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+server = flask.Flask(__name__)
 app = dash.Dash(__name__)
 
 def configure():
@@ -38,6 +40,7 @@ def configure():
 def main():
     configure()
     app.run_server(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
 
 if __name__ == '__main__':
     main()
