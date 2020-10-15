05a0bbd93042613b4e74ee4cbe0330e7372228bb
aambrioso <aambrioso1@gmail.com>
2020-10-14 22:46:14 -0400

    Fix wsgi.py for more basic app.py.

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+def main():
+	app.run(debug=True,port=5000)
+
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
