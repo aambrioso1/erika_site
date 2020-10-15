eb2b6be285bfb0bf9c9ed43b37eaf0410f0a46a0
aambrioso <aambrioso1@gmail.com>
2020-10-14 22:26:25 -0400

    Improve app.py with picture.

--- app.py
+++ app.py
@@ -1,48 +1,10 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+from flask import Flask, render_template
 
-import flask
-import dash
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
-import plot_module as pm
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Flask(__name__)
 
-server = flask.Flask(__name__)
-app = dash.Dash(__name__)
-
-def configure():
-    # create the layout of our app....this is where we add the routes!
-    app.layout = html.Div([
-        dbc.Container([
-            dbc.Row([
-                dbc.Col(html.H1("Aircraft Tracking", className="mb-2"))
-            ]),
-            dbc.Row([
-                dbc.Col(html.H6(children="Florida Tech Capstone Design Project with Embraer", className="mb-4"))
-            ]),
-            dbc.Row([
-                dbc.Col(dbc.Card(html.H3(children='Plant View',
-                                        className= "text-center text-light bg-dark"), body=True, color="dark"),
-                        className="mb-4")
-            ])
-        
-        ]),    
-        dcc.Graph(figure=pm.fig)
-       
-    ])
-
-
-# hide labels and show the plot
-#fig.show()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	return render_template('index.html')
 
 def main():
-    configure()
-    app.run_server(debug=True, port=5000)
-    # app.run(debug=True, port=5000)
-
-if __name__ == '__main__':
-    main()
-else:
-    configure()
+    app.run(debug=True, port=5000)
